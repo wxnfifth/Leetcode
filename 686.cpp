efd1369953ceb82f9eace5a96dce80d61d2dd13d
wxnfifth <wxnfifth@qq.com>
2018-12-31 14:00:09 +0800

    Update 686.cpp

--- 686.cpp
+++ 686.cpp
@@ -1,2 +1,25 @@
+class Solution {
+public:
+    int repeatedStringMatch(string A, string B) {
+        string t = A;
+        int cnt = 1;
+        while (t.length() < B.length()) {
+            t += A;
+            cnt++;
+        }
+        //repeat until longer than B
+        if (t.find(B) != string::npos) {
+            return cnt; 
+        }
+        //if still cannot, maybe A is abcd B is dabcdabcda, abcdabcdabcd cannot, but one more repeat will solve this situation
+        t += A;
+        cnt++;
+        if (t.find(B) != string::npos) {
+            return cnt; 
+        }
+        return -1;
+    }
+};
+
 https://leetcode.com/articles/repeated-string-match/
 
