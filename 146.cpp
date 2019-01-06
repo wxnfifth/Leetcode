b43f0674f8a002f70202b9206f6387cf798d959d
wxnfifth <wxnfifth@qq.com>
2019-01-06 18:39:59 +0800

    Update 146.cpp

--- 146.cpp
+++ 146.cpp
@@ -1,3 +1,51 @@
+class LRUCache {
+    int capacity_;
+    unordered_map<int, list<pair<int,int>>::iterator> mp;//first is key, second is position in the list
+    list<pair<int,int>> lst;//first is key, second is value
+    
+public:
+    LRUCache(int capacity) {
+        capacity_ = capacity;
+    }
+    
+    int get(int key) {
+        if (mp.count(key) > 0) {
+            int value = mp[key]->second;
+            //put the mp[key] position to last position
+            lst.push_back(make_pair(key, value));
+            //erase origin position in lst
+            lst.erase(mp[key]);
+            mp[key] = prev(lst.end());
+            return value;
+        } else {
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        if (mp.count(key) > 0) {
+            lst.push_back(make_pair(key,value));
+            lst.erase(mp[key]);
+            mp[key] = prev(lst.end());
+        } else {
+            //insert new value
+            lst.push_back(make_pair(key,value));
+            mp[key] = prev(lst.end());
+            if (lst.size() > capacity_) {
+                mp.erase(lst.begin()->first);
+                lst.erase(lst.begin());
+            }
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
 //modified a little bit
 class LRUCache {
     int capacity_;
