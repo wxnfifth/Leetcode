a6d81e04a913877193c2954fd662cc5496e68186
wxnfifth <wxnfifth@qq.com>
2019-01-08 14:11:21 +0800

    Create 518.cpp

---
+++ 518.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        //dp[amount] means how many kind 
+        dp[0] = 1;
+        for (int c:coins) {
+            for (int i = 1; i <= amount; ++i) {
+                if (i - c >= 0) {
+                    dp[i] += dp[i - c];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
