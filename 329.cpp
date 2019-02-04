0c4c9968a1a10d002b5700ff14ceda33baf944f1
wxnfifth <wxnfifth@qq.com>
2019-02-04 22:58:48 +0800

    Update 329.cpp

--- 329.cpp
+++ 329.cpp
@@ -41,3 +41,48 @@ public:
         
     }
 };
+
+
+//now solution
+class Solution {
+    vector<vector<int>> path_len;
+    int m = 0;
+    int n = 0;
+    int dir[4][2] = {{-1, 0}, {1,0},{0,1},{0,-1}};
+    int dfs(int i, int j, vector<vector<int>>& matrix) {
+        if (path_len[i][j] > 1) {
+            return path_len[i][j];
+        }
+        int ans = 1;
+        for (int d = 0; d < 4; ++d) {
+            int ni = i + dir[d][0];
+            int nj = j + dir[d][1];
+            if (ni < 0 || nj < 0 || ni >= m || nj >= n || 
+                matrix[i][j] >= matrix[ni][nj]) {
+                continue;
+            }
+            ans = max(ans, dfs(ni, nj, matrix) + 1);
+        }
+        path_len[i][j] = ans;
+        return ans;
+    }
+    
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        //use dfs to find longest increasing for each position
+        //memory each position
+        m = matrix.size();
+        if (m == 0) {
+            return 0;
+        }
+        n = matrix[0].size();
+        path_len = vector<vector<int>>(m, vector<int>(n, 1));
+        int ans = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ans = max(ans, dfs(i, j, matrix));
+            }
+        }
+        return ans;
+    }
+};
