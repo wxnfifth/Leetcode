52c8ee0341e3b2e2aefde942f79372dbdf39562d
wxnfifth <wxnfifth@qq.com>
2019-02-06 20:46:41 +0800

    Update 228.cpp

--- 228.cpp
+++ 228.cpp
@@ -28,3 +28,27 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        if (nums.size() == 0) {
+            return ans;
+        }
+        int start = nums[0];
+        for (int i = 1; i <= nums.size(); ++i) {
+            if (i == nums.size() || nums[i] > nums[i - 1] + 1) {
+                if (nums[i - 1] == start) {
+                    ans.push_back(to_string(start));
+                } else {
+                    ans.push_back(to_string(start) + "->" + to_string(nums[i - 1]));
+                }
+                if (i < nums.size()) {
+                    start = nums[i];
+                }
+            }
+        }
+        return ans;    
+    }
+};
